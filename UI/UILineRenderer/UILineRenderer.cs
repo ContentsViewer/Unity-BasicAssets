89e3c1ce257bc42534e565fbd604a8a52e6c5d00
IOE <fivetwothreesix@gmail.com>
2020-02-10 16:59:51 +0900

    LineRenderer改良: 線の重なりに対応

--- UI/UILineRenderer/UILineRenderer.cs
+++ UI/UILineRenderer/UILineRenderer.cs
@@ -1,155 +1,96 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class UILineRenderer : MaskableGraphic
 {
-    //public new Material material;
-    //public new Color32 color = new Color32(255, 255, 255, 255);
+    public static readonly int MaxPointCountPerRenderer = 16250;
+    //public static readonly int MaxPointCountPerRenderer = 3;
 
     [Range(0.001f, 100)]
     public float width = 10;
     public List<Vector2> points = new List<Vector2>();
-    
 
-    protected override void Awake()
-    {
-        
-    }
+    List<UILineRendererUnit> rendererUnits = new List<UILineRendererUnit>();
 
     protected override void OnPopulateMesh(VertexHelper vh)
     {
         vh.Clear();
-        
-        UIVertex vert = new UIVertex();
-        vert.color = color;
-        
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            // Pointを定義
-            Vector2? point0;
-            Vector2 point1 = points[i + 0];
-            Vector2 point2 = points[i + 1];
-            Vector2? point3;
-
-            if (i - 1 < 0) point0 = null;
-            else point0 = points[i - 1];
-
-            if (i + 2 > points.Count - 1) point3 = null;
-            else point3 = points[i + 2];
-
-            // 頂点座標を計算
-            var normal1 = CalcNormal(point0, point1, point2);
-            var normal2 = CalcNormal(point1, point2, point3);
-            
-
-            vert.position = point1 - normal1 * width;
-            vert.uv0 = new Vector2(0, 0);
-            vh.AddVert(vert);
-
-
-            vert.position = point2 - normal2 * width;
-            vert.uv0 = new Vector2(1, 0);
-            vh.AddVert(vert);
-
-
-            vert.position = point2 + normal2 * width;
-            vert.uv0 = new Vector2(1, 1);
-            vh.AddVert(vert);
-
-
-            vert.position = point1 + normal1 * width;
-            vert.uv0 = new Vector2(0, 1);
-            vh.AddVert(vert);
-
-            vh.AddTriangle(i * 4 + 0, i * 4 + 1, i * 4 + 2);
-            vh.AddTriangle(i * 4 + 0, i * 4 + 2, i * 4 + 3);
-        }
-
-        //Debug.Log("AA");
     }
 
     void Update()
     {
         SetVerticesDirty();
         SetMaterialDirty();
-        //var vertices = new List<Vector3>();
-        //var triangles = new List<int>();
-        //var uvs = new List<Vector2>();
 
-        //for (int i = 0; i < points.Count - 1; i++)
-        //{
-        //    // Pointを定義
-        //    Vector2? point0;
-        //    Vector2 point1 = points[i + 0];
-        //    Vector2 point2 = points[i + 1];
-        //    Vector2? point3;
+        int neededRendererCount = (points.Count - 1) / (MaxPointCountPerRenderer - 1) + 1;
 
-        //    if (i - 1 < 0) point0 = null;
-        //    else point0 = points[i - 1];
+        for (int i = rendererUnits.Count - 1; i >= 0; i--)
+        {
+            if (!rendererUnits[i])
+            {
+                rendererUnits.RemoveAt(i);
+                //Debug.Log("as");
+            }
+        }
 
-        //    if (i + 2 > points.Count - 1) point3 = null;
-        //    else point3 = points[i + 2];
+        for (; rendererUnits.Count < neededRendererCount;)
+        {
+            var newGameObject = new GameObject();
+            newGameObject.transform.SetParent(transform, false);
+            newGameObject.name = "Unit";
+            rendererUnits.Add(newGameObject.AddComponent<UILineRendererUnit>());
+        }
 
-        //    // 頂点座標を計算
-        //    var normal1 = CalcNormal(point0, point1, point2);
-        //    var normal2 = CalcNormal(point1, point2, point3);
+        for (; rendererUnits.Count > neededRendererCount;)
+        {
+            DestroyImmediate(rendererUnits[rendererUnits.Count - 1].gameObject);
+            rendererUnits.RemoveAt(rendererUnits.Count - 1);
+        }
 
-        //    var vertex = new Vector3();
-        //    vertex = point1 - normal1 * width;
-        //    //vertex.uv0 = new Vector2(0, 0);
-        //    vertices.Add(vertex);
+        //Debug.Log(uiLineRenderers.Count);
 
-        //    vertex = point2 - normal2 * width;
-        //    //vertex.uv0 = new Vector2(1, 0);
-        //    vertices.Add(vertex);
+        int start = 0;
+        int end = 0;
 
-        //    vertex = point2 + normal2 * width;
-        //    //vertex.uv0 = new Vector2(1, 1);
-        //    vertices.Add(vertex);
-
-        //    vertex = point1 + normal1 * width;
-        //    //vertex.uv0 = new Vector2(0, 1);
-        //    vertices.Add(vertex);
-
-        //    triangles.Add(i * 4 + 0);
-        //    triangles.Add(i * 4 + 1);
-        //    triangles.Add(i * 4 + 2);
-
-        //    triangles.Add(i * 4 + 0);
-        //    triangles.Add(i * 4 + 2);
-        //    triangles.Add(i * 4 + 3);
-
-        //    uvs.Add(new Vector2(0, 0));
-        //    uvs.Add(new Vector2(1, 0));
-        //    uvs.Add(new Vector2(1, 1));
-        //    uvs.Add(new Vector2(0, 1));
-        //}
-
-        //// 頂点を設定
-
-        //Mesh mesh = new Mesh();
-        //mesh.SetVertices(vertices);
-        //mesh.SetTriangles(triangles, 0);
-        //mesh.SetUVs(0, uvs);
-        //mesh.RecalculateNormals();
-        //mesh.RecalculateTangents();
-        //canvasRenderer.SetMesh(mesh);
-
-        //material.color = color;
-        //canvasRenderer.SetMaterial(material, Texture2D.whiteTexture);
-
+        // プロパティの同期
+        for (int i = 0; i < rendererUnits.Count; i++)
+        {
+            var renderer = rendererUnits[i];
+            renderer.rectTransform.anchorMin = Vector2.zero;
+            renderer.rectTransform.anchorMax = Vector2.one;
+            renderer.rectTransform.offsetMax = Vector2.zero;
+            renderer.rectTransform.offsetMin = Vector2.zero;
+            renderer.rectTransform.pivot = Vector2.zero;
+            //Debug.Log(renderer.rectTransform.offsetMax);
+            renderer.color = color;
+            renderer.material = material;
+            renderer.width = width;
+            
+            start = end;
+            end = Mathf.Min(start + MaxPointCountPerRenderer - 1, points.Count - 1);
+            SyncPointsRange(points, start, end - start + 1, renderer.points);
+        }
     }
 
-    // 線を太らせる方向を計算する
-    Vector2 CalcNormal(Vector2? prev, Vector2 current, Vector2? next)
+    void SyncPointsRange(List<Vector2> from, int start, int count, List<Vector2> target)
     {
-        var dir = Vector2.zero;
-        if (prev.HasValue) dir += prev.Value - current;
-        if (next.HasValue) dir += current - next.Value;
-        dir = new Vector2(-dir.y, dir.x).normalized;
-        return dir;
-    }
+        for (; target.Count < count;)
+        {
+            target.Add(new Vector2(0.0f, 0.0f));
+        }
 
+        if (target.Count > 0)
+        {
+            target.RemoveRange(count, target.Count - count);
+        }
+
+        for(int i = 0; i < count; i++)
+        {
+            target[i] = from[start + i];
+        }
+    }
 
 }
---
+++ UI/UILineRenderer/UILineRendererUnit.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILineRendererUnit : MaskableGraphic
+{
+    //public new Material material;
+    //public new Color32 color = new Color32(255, 255, 255, 255);
+
+    [Range(0.001f, 100)]
+    public float width = 10;
+    public List<Vector2> points = new List<Vector2>();
+
+
+    protected override void Awake()
+    {
+
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+
+        UIVertex vert = new UIVertex();
+        vert.color = color;
+
+        Vector2? vert0, vert1;
+        vert0 = null;
+        vert1 = null;
+
+        int idx = 0;
+        int vert0idx = 0;
+        int vert1idx = 1;
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            Vector2 point0 = points[i];
+            Vector2 point1 = points[i + 1];
+
+            Vector2 vec0 = point1 - point0;
+            Vector2 norm0 = new Vector2(vec0.y, -vec0.x).normalized;
+
+            if (!vert0.HasValue)
+            {
+                vert0 = point0 + norm0 * width;
+                vert0idx = 0;
+
+                vert.position = vert0.Value;
+                //vert.uv0 = new Vector2(1, 0);
+                vh.AddVert(vert);
+
+                idx++;
+            }
+            if (!vert1.HasValue)
+            {
+                vert1 = point0 - norm0 * width;
+                vert1idx = 1;
+
+                vert.position = vert1.Value;
+                //vert.uv0 = new Vector2(0, 0);
+                vh.AddVert(vert);
+
+                idx++;
+            }
+
+            var point10r = point1 + norm0 * width;
+            var point10l = point1 - norm0 * width;
+
+            Vector2 vert2 = Vector2.zero, vert3 = Vector2.zero, vert4 = Vector2.zero;
+
+            if (i + 2 < points.Count)
+            {
+                Vector2 point2 = points[i + 2];
+
+                Vector2 vec1 = point2 - point1;
+                Vector2 norm1 = new Vector2(vec1.y, -vec1.x).normalized;
+
+                var point11r = point1 + norm1 * width;
+                var point11l = point1 - norm1 * width;
+
+                var point21r = point2 + norm1 * width;
+                var point21l = point2 - norm1 * width;
+
+                //Vector2 vert4uv = new Vector2(0, 0);
+
+                bool isTwistedRight = false;
+                Vector2 intersection;
+                if (CalLineSegmentsIntersection(vert0.Value, point10r, point11r, point21r, out intersection))
+                {
+                    isTwistedRight = true;
+                    vert3 = intersection;
+                }
+                else
+                {
+                    vert3 = point10r;
+                    vert4 = point11r;
+                    //vert4uv = new Vector2(0, 1);
+                }
+
+                if (CalLineSegmentsIntersection(vert1.Value, point10l, point11l, point21l, out intersection))
+                {
+                    isTwistedRight = false;
+                    vert2 = intersection;
+                }
+                else
+                {
+                    vert2 = point10l;
+                    vert4 = point11l;
+                    //vert4uv = new Vector2(1, 1);
+                }
+
+
+                vert.position = vert2;
+                //vert.uv0 = new Vector2(0, 1);
+                vh.AddVert(vert);
+
+                vert.position = vert3;
+                //vert.uv0 = new Vector2(1, 1);
+                vh.AddVert(vert);
+
+                vert.position = vert4;
+                //vert.uv0 = vert4uv;
+                vh.AddVert(vert);
+
+                // vert2 index: idx
+                // vert3 index: idx + 1
+                // vert4 index: idx + 2
+
+                vh.AddTriangle(vert0idx, vert1idx, idx);    // vert0, vert1, vert2
+                vh.AddTriangle(vert0idx, idx, idx + 1);     // vert0, vert2, vert3
+                vh.AddTriangle(idx, idx + 2, idx + 1);      // vert2, vert4, vert3
+
+
+                if (isTwistedRight)
+                {
+                    vert0 = vert3;
+                    vert0idx = idx + 1;
+
+                    vert1 = vert4;
+                    vert1idx = idx + 2;
+                }
+                else
+                {
+                    vert0 = vert4;
+                    vert0idx = idx + 2;
+
+                    vert1 = vert2;
+                    vert1idx = idx;
+                }
+
+                idx += 3;
+            }
+            else
+            {
+
+                vert.position = point10l;
+                //vert.uv0 = new Vector2(1, 1);
+                vh.AddVert(vert);
+
+                vert.position = point10r;
+                //vert.uv0 = new Vector2(0, 1);
+                vh.AddVert(vert);
+
+
+                vh.AddTriangle(vert0idx, vert1idx, idx);    // vert0, vert1, point10l
+                vh.AddTriangle(vert0idx, idx, idx + 1);     // vert0, point10l, point10r
+
+                idx += 2;
+            }
+
+            //// Pointを定義
+            //Vector2? point0;
+            //Vector2 point1 = points[i + 0];
+            //Vector2 point2 = points[i + 1];
+            //Vector2? point3;
+
+            //if (i - 1 < 0) point0 = null;
+            //else point0 = points[i - 1];
+
+            //if (i + 2 > points.Count - 1) point3 = null;
+            //else point3 = points[i + 2];
+
+
+            //// 頂点座標を計算
+            //var normal1 = CalcNormal(point0, point1, point2);
+            //var normal2 = CalcNormal(point1, point2, point3);
+
+
+            //vert.position = point1 - normal1 * width;
+            //vert.uv0 = new Vector2(0, 0);
+            //vh.AddVert(vert);
+
+
+            //vert.position = point2 - normal2 * width;
+            //vert.uv0 = new Vector2(1, 0);
+            //vh.AddVert(vert);
+
+
+            //vert.position = point2 + normal2 * width;
+            //vert.uv0 = new Vector2(1, 1);
+            //vh.AddVert(vert);
+
+
+            //vert.position = point1 + normal1 * width;
+            //vert.uv0 = new Vector2(0, 1);
+            //vh.AddVert(vert);
+
+            //vh.AddTriangle(i * 4 + 0, i * 4 + 1, i * 4 + 2);
+            //vh.AddTriangle(i * 4 + 0, i * 4 + 2, i * 4 + 3);
+        }
+
+    }
+
+    void Update()
+    {
+        SetVerticesDirty();
+        SetMaterialDirty();
+    }
+
+
+    bool CalLineSegmentsIntersection(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, out Vector2 intersection)
+    {
+        intersection = Vector2.zero;
+
+        var d = (p1.x - p0.x) * (p3.y - p2.y) - (p1.y - p0.y) * (p3.x - p2.x);
+        if (d == 0.0f)
+        {
+            return false;
+        }
+
+        var u = ((p2.x - p0.x) * (p3.y - p2.y) - (p2.y - p0.y) * (p3.x - p2.x)) / d;
+        var v = ((p2.x - p0.x) * (p1.y - p0.y) - (p2.y - p0.y) * (p1.x - p0.x)) / d;
+
+        if (u < 0.0f || u > 1.0f || v < 0.0f || v > 1.0f)
+        {
+            return false;
+        }
+
+        intersection.x = p0.x + u * (p1.x - p0.x);
+        intersection.y = p0.y + u * (p1.y - p0.y);
+
+        return true;
+    }
+
+}
